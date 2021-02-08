91cc7ffadaf578e9a1b18fba668c1535f5703dec
Can Soylu <hsyncn17@gmail.com>
2021-02-08 18:08:53 +0300

    added new function

--- day6.js
+++ day6.js
@@ -3,21 +3,15 @@ function changeFirstHeading() {
   firstHeading.innerText = "Hello Strive!";
 }
 
-changeFirstHeading();
-
 function changeBackgroundColor() {
   let backGround = document.querySelector("body");
   backGround.style.backgroundColor = "#ffe100";
 }
 
-changeBackgroundColor();
-
 function changeFooter() {
   let fakeFooter = document.getElementById("fake");
   fakeFooter.innerText = "this is a fake adress";
 }
-
-changeFooter();
 
 function hideİmg() {
   let imageHide = document.getElementsByClassName("hide");
@@ -26,8 +20,6 @@ function hideİmg() {
   }
 }
 
-hideİmg();
-
 function changeColor() {
   let colorChange = document.getElementsByClassName("color");
   for (let i = 0; i < colorChange.length; i++) {
@@ -35,5 +27,3 @@ function changeColor() {
       "#" + Math.floor(Math.random() * 16777215).toString(16);
   }
 }
-
-changeColor();
